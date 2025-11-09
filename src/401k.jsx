a91279cb622ca2ebf0be17ded1386ac09622f69a
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-08 21:22:38 -0800

    Fixed some naming, built table

--- src/401k.jsx
+++ src/401k.jsx
@@ -1,6 +1,31 @@
-function _401k () {
+function _401k ({offer1, offer2}) {
     return (
-        <div></div>
+        <div>
+            <table>
+                <thead>
+                    <th>Provision</th>
+                    <th>{/* offer1.companyName */}</th>
+                    <th>{/* offer2.companyName */}</th>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Company Match</td>
+                    <td>{/* offer1.k401.companyMatch */}</td>
+                    <td>{/* offer2.k401.companyMatch */}</td>
+                </tr>
+                <tr>
+                    <td>Non-elective Contribution</td>
+                    <td>{/* offer1.k401.nonElective */}</td>
+                    <td>{/* offer2.k401.nonElective */}</td>
+                </tr>
+                <tr>
+                    <td>Vesting Period</td>
+                    <td>{/* offer1.k401.vesting */}</td>
+                    <td>{/* offer2.k401.vesting */}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
