c2d42d8aa7edd5786ffa022ac338d61e4e2e09f2
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-17 17:06:05 -0800

    Fixed syntax (hopefully) and uncommented js parts

--- src/401k.jsx
+++ src/401k.jsx
@@ -10,37 +10,33 @@ function _401k ({offer1, offer2}) {
             <table>
                 <thead>
                     <th>Provision</th>
-                    <th>{/* offer1.companyName */}</th>
-                    <th>{/* offer2.companyName */}</th>
+                    <th>{ offer1.companyName }</th>
+                    <th>{ offer2.companyName }</th>
                 </thead>
                 <tbody>
                 <tr>
                     <td>Company Match</td>
-                    <td {/* style= {hilight(offer1.K401.companyMatch, offer2.K401.companyMatch)} */}>
-                        {/* offer1.K401.companyMatch */}
+                    <td style= {hilight(offer1.K401.companyMatch, offer2.K401.companyMatch)}>
+                        { offer1.K401.companyMatch }
                     </td>
-                    <td {/* style= {hilight(offer2.K401.companyMatch, offer1.K401.companyMatch)} */}>
-                        {/* offer2.K401.companyMatch */}
+                    <td style= {hilight(offer2.K401.companyMatch, offer1.K401.companyMatch)}>
+                        { offer2.K401.companyMatch }
                     </td>
                 </tr>
                 <tr>
                     <td>Non-elective Contribution</td>
-                    <td {/* style= {hilight(offer1.K401.nonElective, offer2.K401.nonElective)} */}>
-                        {/* offer1.K401.nonElective */}
+                    <td style= {hilight(offer1.K401.nonElective, offer2.K401.nonElective)}>
+                        { offer1.K401.nonElective }
                     </td>
-                    <td {/* style= {hilight(offer2.K401.nonElective, offer1.K401.nonElective)} */}>
-                        {/* offer2.K401.nonElective */}
+                    <td style= {hilight(offer2.K401.nonElective, offer1.K401.nonElective)}>
+                        { offer2.K401.nonElective }
                     </td>
                 </tr>
-                <tr>
+                <td>
                     <td>Vesting Period</td>
-                    <td {/* style= {hilight(offer1.K401.vesting, offer2.K401.vesting)} */}>
-                        {/* offer1.K401.vesting */}
-                    </td>
-                    <td {/* style= {hilight(offer2.K401.vesting, offer1.K401.vesting)} */}>
-                        {/* offer2.K401.vesting */}
-                    </td>
-                </tr>
+                    <td style={hilight(offer1.K401.vesting, offer2.K401.vesting)}> {offer1.K401.vesting} </td>
+                    <td style={hilight(offer2.K401.vesting, offer1.K401.vesting)}> {offer2.K401.vesting} </td>
+                </td>
                 </tbody>
             </table>
         </div>
